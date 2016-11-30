6ac7118176d0083a1dc35e9bfb115720f69fcdb4
entcheva <christina.entcheva@gmail.com>
2016-11-30 01:22:13 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name=(names) #writer
+    @name = names
+  end
+
+  def name #reader
+    @name
+  end
+
+  def breed=(breeds) #writer
+    @breed = breeds
+  end
+
+  def breed #reader
+    @breed
+  end
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name=(names) #writer
+    @name = names
+  end
+
+  def name #reader
+    @name
+  end
+
+  def job=(jobs) #writer
+    @job = jobs
+  end
+
+  def job #reader
+    @job
+  end
+
+
+end
